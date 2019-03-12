47566eeed35e73c86708d50cba53ecab7d440df0
Weijie Ye <P17226039@my365.dmu.ac.uk>
2019-03-12 01:15:49 +0000

    finished staff collection

--- ClassLibrary/clsStaffCollection.cs
+++ ClassLibrary/clsStaffCollection.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace ClassLibrary
 {
@@ -6,7 +7,55 @@ namespace ClassLibrary
     {
         //private data member for the list
         List<clsStaff> mStaffList = new List<clsStaff>();
+        //private data member thisStaff
+        clsStaff mThisStaff = new clsStaff();
 
+        //constructor for the class
+        public clsStaffCollection()
+        {
+            //object for data connection
+            clsDataConnection DB = new clsDataConnection();
+            //execute the stored procedure
+            DB.Execute("sproc_tblStaff_SelectAll");
+            //populate the array list with the data table
+            PopulateArray(DB);
+        }
+
+        void PopulateArray(clsDataConnection DB)
+        {
+            //populates the array list based on the data table in the parameter DB
+            //var for the index
+            Int32 Index = 0;
+            //var to store the record count
+            Int32 RecordCount = 0;           
+            //get the count of records
+            RecordCount = DB.Count;
+            //clear the private array list
+            mStaffList = new List<clsStaff>();
+            //while there are records to process
+            while (Index < RecordCount)
+            {
+                //create a blank staff
+                clsStaff AStaff = new clsStaff();
+                //read in the fields from the current record
+                AStaff.StaffNo = Convert.ToInt32(DB.DataTable.Rows[Index]["StaffNo"]);
+                AStaff.FirstName = Convert.ToString(DB.DataTable.Rows[Index]["FirstName"]);
+                AStaff.Surname = Convert.ToString(DB.DataTable.Rows[Index]["Surname"]);
+                AStaff.Sex = Convert.ToString(DB.DataTable.Rows[Index]["Sex"]);
+                AStaff.DOB = Convert.ToDateTime(DB.DataTable.Rows[Index]["DOB"]);
+                AStaff.Address1 = Convert.ToString(DB.DataTable.Rows[Index]["Address1"]);
+                AStaff.Address2 = Convert.ToString(DB.DataTable.Rows[Index]["Address2"]);
+                AStaff.Address3 = Convert.ToString(DB.DataTable.Rows[Index]["Address3"]);
+                AStaff.PostCode = Convert.ToString(DB.DataTable.Rows[Index]["PostCode"]);
+                AStaff.Email = Convert.ToString(DB.DataTable.Rows[Index]["Email"]);
+                AStaff.Phone = Convert.ToString(DB.DataTable.Rows[Index]["Phone"]);
+                //add the record to the private data member
+                mStaffList.Add(AStaff);
+                //point at the nect record
+                Index++;
+            }
+        }
+               
         //public property for the address list
         public List<clsStaff> StaffList
         {
@@ -36,6 +85,85 @@ namespace ClassLibrary
             }
         }
 
-        public clsStaff ThisStaff { get; set; }
+        //public property for ThisStaff
+        public clsStaff ThisStaff
+        {
+            get
+            {
+                //return the private data
+                return mThisStaff;
+            }
+            set
+            {
+                //set the private data
+                mThisStaff = value;
+            }
+        }
+
+        public int Add()
+        {
+            //adds a new record to the database based on the values of thisStaff
+            //connect to the database
+            clsDataConnection DB = new clsDataConnection();
+            //set the parameters for the stored procedure
+            DB.AddParameter("FirstName", mThisStaff.FirstName);
+            DB.AddParameter("Surname", mThisStaff.Surname);
+            DB.AddParameter("Sex", mThisStaff.Sex);
+            DB.AddParameter("DOB", mThisStaff.DOB);
+            DB.AddParameter("Address1", mThisStaff.Address1);
+            DB.AddParameter("Address2", mThisStaff.Address2);
+            DB.AddParameter("Address3", mThisStaff.Address3);
+            DB.AddParameter("PostCode", mThisStaff.PostCode);
+            DB.AddParameter("Email", mThisStaff.Email);
+            DB.AddParameter("Phone", mThisStaff.Phone);
+            //execute the query returning the primary key value
+            return DB.Execute("sproc_tblStaff_Insert");
+        }
+
+        public void Delete()
+        {
+            //deletes the record pointed to by thisStaff
+            //coonect to the database
+            clsDataConnection DB = new clsDataConnection();
+            //set the parameters for the stored procedure
+            DB.AddParameter("@StaffNo", mThisStaff.StaffNo);
+            //execute the stored procedure
+            DB.Execute("sproc_tblStaff_Delete");
+
+        }
+
+        public void ReportByPostCode(string PostCode)
+        {
+            //filters the records based on a full or partial post code
+            //connect to the database
+            clsDataConnection DB = new clsDataConnection();
+            //send the PostCode parameter to the database
+            DB.AddParameter("@PostCode", PostCode);
+            //execute the stored procedure
+            DB.Execute("sproc_tblStaff_FilterByPostCode");
+            //populate the array list with the data table
+            PopulateArray(DB);
+        }
+
+        public void Update()
+        {
+            //update an existing record based on the values of thisAddress
+            //connect to the database
+            clsDataConnection DB = new clsDataConnection();
+            //set the parameters for the stored procedure
+            DB.AddParameter("StaffNo", mThisStaff.StaffNo);
+            DB.AddParameter("FirstName", mThisStaff.FirstName);
+            DB.AddParameter("Surname", mThisStaff.Surname);
+            DB.AddParameter("Sex", mThisStaff.Sex);
+            DB.AddParameter("DOB", mThisStaff.DOB);
+            DB.AddParameter("Address1", mThisStaff.Address1);
+            DB.AddParameter("Address2", mThisStaff.Address2);
+            DB.AddParameter("Address3", mThisStaff.Address3);
+            DB.AddParameter("PostCode", mThisStaff.PostCode);
+            DB.AddParameter("Email", mThisStaff.Email);
+            DB.AddParameter("Phone", mThisStaff.Phone);
+            //execute the stored procedure
+            DB.Execute("sproc_tblStaff_Update");
+        }
     }
 }
--- RestaurantTesting/tstStaffCollection.cs
+++ RestaurantTesting/tstStaffCollection.cs
@@ -118,12 +118,173 @@ namespace RestaurantTesting
         }
 
         [TestMethod]
-        public void TwoRecordsPresent()
+        public void AddMethodOK()
         {
             //create an instance of the class we want to create
             clsStaffCollection AllStaffs = new clsStaffCollection();
+            //create the item of the test data
+            clsStaff TestItem = new clsStaff();
+            //var to store the primary key
+            Int32 PrimaryKey = 0;
+            TestItem.StaffNo = 1;
+            TestItem.FirstName = "Some FirstName";
+            TestItem.Surname = "Some Surname";
+            TestItem.Sex = "Some Sex";
+            TestItem.DOB = DateTime.Now.Date;
+            TestItem.Address1 = "Some Address1";
+            TestItem.Address2 = "Some Address2";
+            TestItem.Address3 = "Some Address3";
+            TestItem.PostCode = "Some PostCode";
+            TestItem.Email = "Some Email";
+            TestItem.Phone = "Some Phone";
+            //set ThisStaff to the test data
+            AllStaffs.ThisStaff = TestItem;
+            //add the record
+            PrimaryKey = AllStaffs.Add();
+            //set the primary key of the test data
+            TestItem.StaffNo = PrimaryKey;
+            //find the record
+            AllStaffs.ThisStaff.Find(PrimaryKey);
             //test to see that the two values are the same
-            Assert.AreEqual(AllStaffs.Count, 2);
+            Assert.AreEqual(AllStaffs.ThisStaff, TestItem);
+        }
+
+        [TestMethod]
+        public void DeleteMethodOK()
+        {
+            //create an instance of the class we want to create
+            clsStaffCollection AllStaffs = new clsStaffCollection();
+            //create the item of the test data
+            clsStaff TestItem = new clsStaff();
+            //var to store the primary key
+            Int32 PrimaryKey = 0;
+            TestItem.StaffNo = 1;
+            TestItem.FirstName = "Some FirstName";
+            TestItem.Surname = "Some Surname";
+            TestItem.Sex = "Some Sex";
+            TestItem.DOB = DateTime.Now.Date;
+            TestItem.Address1 = "Some Address1";
+            TestItem.Address2 = "Some Address2";
+            TestItem.Address3 = "Some Address3";
+            TestItem.PostCode = "Some PostCode";
+            TestItem.Email = "Some Email";
+            TestItem.Phone = "Some Phone";
+            //set ThisStaff to the test data
+            AllStaffs.ThisStaff = TestItem;
+            //add the record
+            PrimaryKey = AllStaffs.Add();
+            //set the primary key of the test data
+            TestItem.StaffNo = PrimaryKey;
+            //find the record
+            AllStaffs.ThisStaff.Find(PrimaryKey);
+            //delete the record
+            AllStaffs.Delete();
+            //now find the record
+            Boolean Found = AllStaffs.ThisStaff.Find(PrimaryKey);
+            //test to see that the record was not found
+            Assert.IsFalse(Found);
+        }
+
+        [TestMethod]
+        public void UpdateMethodOK()
+        {
+            //create an instance of the class we want to create
+            clsStaffCollection AllStaffs = new clsStaffCollection();
+            //create the item of the test data
+            clsStaff TestItem = new clsStaff();
+            //var to store the primary key
+            Int32 PrimaryKey = 0;
+            TestItem.StaffNo = 1;
+            TestItem.FirstName = "Some FirstName";
+            TestItem.Surname = "Some Surname";
+            TestItem.Sex = "Some Sex";
+            TestItem.DOB = DateTime.Now.Date;
+            TestItem.Address1 = "Some Address1";
+            TestItem.Address2 = "Some Address2";
+            TestItem.Address3 = "Some Address3";
+            TestItem.PostCode = "Some PostCode";
+            TestItem.Email = "Some Email";
+            TestItem.Phone = "Some Phone";
+            //set ThisStaff to the test data
+            AllStaffs.ThisStaff = TestItem;
+            //add the record
+            PrimaryKey = AllStaffs.Add();
+            //set the primary key of the test data
+            TestItem.StaffNo = PrimaryKey;
+            //modify the test data
+            TestItem.FirstName = "Some FirstName 1";
+            TestItem.Surname = "Some Surname 1";
+            TestItem.Sex = "Some Sex 1";
+            TestItem.DOB = DateTime.Now.Date;
+            TestItem.Address1 = "Some Address4";
+            TestItem.Address2 = "Some Address5";
+            TestItem.Address3 = "Some Address6";
+            TestItem.PostCode = "Some PostCode 1";
+            TestItem.Email = "Some Email 1";
+            TestItem.Phone = "Some Phone 1";
+            //set the record based on the new test data
+            AllStaffs.ThisStaff = TestItem;
+            //update the record
+            AllStaffs.Update();
+            //find the record
+            AllStaffs.ThisStaff.Find(PrimaryKey);
+            //test to see that the record was not found
+            Assert.AreEqual(AllStaffs.ThisStaff, TestItem);
+        }
+
+        [TestMethod]
+        public void ReportByPostCodeMethodOK()
+        {
+            //create an instance of the class containing unfiltered results
+            clsStaffCollection AllStaffs = new clsStaffCollection();
+            //create an instance of the filtered data
+            clsStaffCollection FilteredStaffs = new clsStaffCollection();
+            //apply a blank string (should return all records);
+            FilteredStaffs.ReportByPostCode("");
+            //test to see that the two values are the same
+            Assert.AreEqual(AllStaffs.Count, FilteredStaffs.Count);
+        }
+
+        [TestMethod]
+        public void ReportByPostCodeNoneFound()
+        {
+            //create an instance of the filtered data
+            clsStaffCollection FilteredStaffs = new clsStaffCollection();
+            //apply a post code that doesn't exist
+            FilteredStaffs.ReportByPostCode("xxx xxx");
+            //test to see that there are no records
+            Assert.AreEqual(0, FilteredStaffs.Count);
+        }
+
+        [TestMethod]
+        public void ReportByPostCodeTestDataFound()
+        {
+            //create an instance of the filtered data
+            clsStaffCollection FilteredStaffs = new clsStaffCollection();
+            //var to store outcome
+            Boolean OK = true;
+            //apply a post code that doesn't exist
+            FilteredStaffs.ReportByPostCode("yyy yyy");
+            //check that the correct number of records are found
+            if (FilteredStaffs.Count == 2)
+            {
+                //check that the first record is ID 36
+                if (FilteredStaffs.StaffList[0].StaffNo != 36)
+                {
+                    OK = false;
+                }
+                //check that the first record is ID 37
+                if (FilteredStaffs.StaffList[1].StaffNo != 37)
+                {
+                    OK = false;
+                }
+            }
+            else
+            {
+                OK = false;
+            }
+            //test to see that there are no records
+            Assert.IsTrue(OK);
         }
     }
 }
